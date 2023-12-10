c5d57228cbc03fb92a9df89c77e686d4c78266f1
kemkai <kemkaikub@gmail.com>
2023-12-10 18:38:41 +0700

    bruh

--- NewYear.java
+++ NewYear.java
@@ -13,6 +13,7 @@ public class NewYear extends JPanel{
         f.setSize(600, 600);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
+        f.setResizable(false);
     }
 
     public void paintComponent(Graphics g) {
@@ -30,13 +31,13 @@ public class NewYear extends JPanel{
     
     private static void sky(Graphics g) {
         g.setColor(new Color(9, 30, 60));
-        g.fillRect(0, 0, 600, 300);
+        g.fillRect(0, 0, 600, 350);
     }
     
     private static void grass(Graphics g) {
     // g.setColor(new Color(128, 192, 20));
         g.setColor(Color.decode("#80C014"));
-        g.fillRect(0, 300, 600, 600);
+        g.fillRect(0, 350, 600, 240);
         
     }
 
@@ -44,7 +45,21 @@ public class NewYear extends JPanel{
         // body
         g.setColor(Color.decode("#AF6329"));
         // g.drawRoundRect(1,460 ,300 , 20 ,10 , 10);
-        g.fillRoundRect(1, 460, 300, 20, 10, 10);
+        g.fillRoundRect(1, 460-30, 300, 20, 10, 15);
+
+        // left leg
+        g.fillRect(15, 480-30, 20, 120);
+
+        // middle part
+        g.fillRect(30, 510-30, 160, 20);
+
+        // red box
+        g.setColor(Color.decode("#EC2C39"));
+        g.fillRect(190, 480-30, 85, 120);
+
+        // half box
+        g.setColor(Color.decode("#AF6329"));
+        g.fillRect(190, (480+120/2)-30, 85, 5);
     }
     
 }
